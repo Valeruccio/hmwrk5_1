ef21d5f236d7f4d69b40c029eb47b93be30b2c94
Valeriy Tiulin <kugelprint@gmail.com>
2021-03-11 19:08:29 +0300

    Задачки

---
+++ mission2.py
@@ -0,0 +1,6 @@
+with open('file_2.txt', 'r') as my_f:
+    content = my_f.readlines()
+    print(f'Количество строк: {len(content)}')
+    for i in range(len(content)):
+
+        print(f'Количество слов: {i + 1} строке - {len(content[i].split())}')
